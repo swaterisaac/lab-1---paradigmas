a02f19f6bacce9bd344467766364f3aaa6d1f831
swaterisaac <isaac.rojas@usach.cl>
2019-09-11 00:54:13 -0300

    Agregado constructor y pertenencia floor

---
+++ proyectoParadigma.bak
@@ -0,0 +1,7 @@
+#lang racket
+;createScene:
+;Dom: cuatro números enteros positivos y un número entero.
+;N y M indican el tamaño del escenario.
+;E la cantidad de enemigos.
+;D indica la la dificultad.
+;seed indica la semilla de la escena.
--- proyectoParadigma.rkt
+++ proyectoParadigma.rkt
@@ -1,4 +1,29 @@
 #lang racket
+;Suelo: El suelo de la escena, en donde se paran los jugadores y enemigos.}
+
+;createFloor: parámetros (X Y)
+;desc: Función constructora de floor
+;dom:entero X entero
+;X: Coordenada en X (nivel en el suelo)
+;Y: Coordenada en Y (altura)
+;rec: Floor. ('F' X Y)
+(define (createFloor X Y)
+  (list "F" X Y))
+;floor?: parámetros (algo)
+;desc: Función de pertenencia de floor
+;dom: cualquier tipo de dato
+;rec: Booleano.
+(define (floor? F)
+  (if (list? F)
+      (if (and
+           (and
+               (equal? (car F) "F")
+               (and (number? (car (cdr F))) (>= (car (cdr F)) 0)))
+               (and (number? (car (cdr F))) (>= (car (cdr F)) 0)))
+          #t
+          #f)
+      #f))
+                
 ;createScene:
 ;Dom: cuatro números enteros positivos y un número entero.
 ;N y M indican el tamaño del escenario.
