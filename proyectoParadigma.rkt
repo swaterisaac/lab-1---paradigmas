fc1632798a302588ceec00815ac32c82de29fa36
swaterisaac <isaac.rojas@usach.cl>
2019-09-11 01:49:04 -0300

    Incluido constructor y pertenencia de player, y se modificó algunos elementos de las funciones de floor

--- proyectoParadigma.rkt
+++ proyectoParadigma.rkt
@@ -1,4 +1,16 @@
 #lang racket
+;intPositive?: Parámetros (algo)
+;desc: Comprueba si un número es entero positivo (aunque incluye el 0)
+;dom: Cualquier tipo de dato
+;rec: Booleano
+(define (intPositive? x)
+  (if (integer? x)
+      (if (>= x 0)
+          #t
+          #f)
+      #f)
+  )
+
 ;Suelo: El suelo de la escena, en donde se paran los jugadores y enemigos.}
 
 ;createFloor: parámetros (X Y)
@@ -8,7 +20,11 @@
 ;Y: Coordenada en Y (altura)
 ;rec: Floor. ('F' X Y)
 (define (createFloor X Y)
-  (list "F" X Y))
+  (if (and (intPositive? X)
+           (intPositive? Y))
+      (list "F" X Y)
+      null)
+  )
 ;floor?: parámetros (algo)
 ;desc: Función de pertenencia de floor
 ;dom: cualquier tipo de dato
@@ -16,13 +32,65 @@
 (define (floor? F)
   (if (list? F)
       (if (and
-           (and
                (equal? (car F) "F")
-               (and (number? (car (cdr F))) (>= (car (cdr F)) 0)))
-               (and (number? (car (cdr F))) (>= (car (cdr F)) 0)))
+               (intPositive? (car (cdr F)))
+               (intPositive? (car (cdr (cdr F))))
+               )
           #t
           #f)
       #f))
+;getX: parámetros (floor)
+;desc: Recibe un floor y entrega la coordenada X.
+;dom: floor
+;rec: Coordenada X del floor.
+
+;getY: parámetros (floor)
+;desc: Recibe un floor y entrega la coordenada Y.
+;dom: floor
+;rec: Coordenada Y del floor.
+
+
+;Player
+
+;createPlayer: parámetros (X Y angle)
+;desc: Función constructora de player. OBS: Suma 1 de altura ya que ocupa 2 espacios.
+;dom: entero X entero X num
+;X: Coordenada en X (suelo)
+;Y: Coordenada más baja en Y (altura)
+;angle: Ángulo con el que mira el player (0 a 359)
+;rec: player. ("P" X Y1 Y2 angle)
+(define (createPlayer X Y angle)
+  (if (and (intPositive? X)
+           (intPositive? Y)
+           (number? angle))
+      (list "P" X Y (+ Y 1) (modulo angle 360))
+      null)
+  )
+;player?: parámetros (player)
+;desc: Función de pertenencia de player.
+;dom: Cualquier dato
+;rec: booleano
+(define (player? P)
+  (if (list? P)
+      (if (and (= (car P) "P")
+               (intPositive? (car (cdr P)))
+               (intPositive? (car (cdr (cdr P))))
+               (intPositive? (car (cdr (cdr (cdr P)))))
+               (intPositive? (car (cdr (cdr (cdr (cdr P))))))
+               )
+          #t
+          #f)
+      #f)
+  )
+;getPlayerX: parámetros (player)
+;desc: Función selectora de player que saca la coordenada X.
+;dom: player
+;rec: coordenada X de player (numero entero positivo incluyendo 0)
+
+;getPlayerY: parámetros (player)
+;desc: Función selectora de player que saca la coordenada Y.
+;dom: player
+;rec: coordenada Y de player número entero positivo incluyendo 0)
                 
 ;createScene:
 ;Dom: cuatro números enteros positivos y un número entero.
